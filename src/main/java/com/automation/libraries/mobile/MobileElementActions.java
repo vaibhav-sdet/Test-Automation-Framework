fb9f62c9a35d12bc262f2103eaf57f690075c6ac
Vaibhav Tiwari <113414@ecomexpress.in>
2025-02-20 11:53:24 +0530

    Adding Mobile element Action class

---
+++ src/main/java/com/automation/libraries/mobile/MobileElementActions.java
@@ -0,0 +1,76 @@
+package com.automation.libraries.mobile;
+
+
+import com.automation.libraries.common.ElementActions;
+import io.appium.java_client.AppiumDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.interactions.PointerInput;
+import org.openqa.selenium.interactions.Sequence;
+
+import java.time.Duration;
+import java.util.Collections;
+
+public class MobileElementActions extends ElementActions {
+
+    private final AppiumDriver driver;
+
+    public MobileElementActions(AppiumDriver driver) {
+        super(driver);
+        this.driver = driver;
+    }
+
+    public void tap(WebElement element) {
+        element.click();
+    }
+
+    public void longPress(WebElement element) {
+        new Actions(driver)
+                .clickAndHold(element)
+                .pause(Duration.ofSeconds(2))
+                .release()
+                .perform();
+    }
+
+    public void swipe(int startX, int startY, int endX, int endY) {
+        PointerInput finger = new PointerInput(PointerInput.Kind.TOUCH, "finger");
+        Sequence swipe = new Sequence(finger, 0);
+
+        swipe.addAction(finger.createPointerMove(Duration.ZERO, PointerInput.Origin.viewport(), startX, startY));
+        swipe.addAction(finger.createPointerDown(PointerInput.MouseButton.LEFT.asArg()));
+        swipe.addAction(finger.createPointerMove(Duration.ofMillis(500), PointerInput.Origin.viewport(), endX, endY));
+        swipe.addAction(finger.createPointerUp(PointerInput.MouseButton.LEFT.asArg()));
+
+        driver.perform(Collections.singletonList(swipe));
+    }
+
+    public void scrollDown() {
+        int startX = driver.manage().window().getSize().width / 2;
+        int startY = (int) (driver.manage().window().getSize().height * 0.7);
+        int endY = (int) (driver.manage().window().getSize().height * 0.3);
+        swipe(startX, startY, startX, endY);
+    }
+
+    public void scrollUp() {
+        int startX = driver.manage().window().getSize().width / 2;
+        int startY = (int) (driver.manage().window().getSize().height * 0.3);
+        int endY = (int) (driver.manage().window().getSize().height * 0.7);
+        swipe(startX, startY, startX, endY);
+    }
+
+    public void pinch(WebElement element) {
+        new Actions(driver)
+                .clickAndHold(element)
+                .moveByOffset(-10, -10)
+                .release()
+                .perform();
+    }
+
+    public void zoom(WebElement element) {
+        new Actions(driver)
+                .clickAndHold(element)
+                .moveByOffset(10, 10)
+                .release()
+                .perform();
+    }
+}
