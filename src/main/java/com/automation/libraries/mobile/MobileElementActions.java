ab7cb2f570fea0546f2e4605d5b51df577360001
Vaibhav Tiwari <113414@ecomexpress.in>
2025-02-21 16:55:28 +0530

    Adding Generic Swipe action

--- src/main/java/com/automation/libraries/mobile/MobileElementActions.java
+++ src/main/java/com/automation/libraries/mobile/MobileElementActions.java
@@ -88,4 +88,15 @@ public class MobileElementActions extends ElementActions {
             ));
         }while(canScrollMore);
     }
+
+
+    public void swipeAction(WebElement ele,String direction)
+    {
+        ((JavascriptExecutor) driver).executeScript("mobile: swipeGesture", ImmutableMap.of(
+                "elementId", ((RemoteWebElement)ele).getId(),
+
+                "direction", direction,
+                "percent", 0.75
+        ));
+    }
 }
